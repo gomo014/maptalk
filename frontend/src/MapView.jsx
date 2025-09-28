9b218bf1b9a4ae0796edaffad13864bcd13c4f9c
gomo014 <afgmjkl3@gmail.com>
2025-09-28 22:08:34 +0900

    Add map display and marker placement on click

--- frontend/src/MapView.jsx
+++ frontend/src/MapView.jsx
@@ -1,17 +1,41 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+
+function ClickableMap({ onClick }) {
+  useMapEvents({
+    click(e) {
+      onClick(e.latlng);
+    },
+  });
+  return null;
+}
 
 function MapView() {
+  const [marker, setMarker] = useState(null);
+
+  const handleMapClick = (latlng) => {
+    setMarker(latlng);
+    // APIに保存処理を追加
+  };
+
   return (
-    <MapContainer center={[35.6895, 139.6917]} zoom={13} style={{ height: "100vh", width: "100vw" }}>
+    <MapContainer
+      center={[35.6895, 139.6917]}
+      zoom={13}
+      style={{ height: "100vh", width: "100vw" }}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
-      <Marker position={[35.6895, 139.6917]}>
-        <Popup>
-          東京
-        </Popup>
-      </Marker>
+      <ClickableMap onClick={handleMapClick} />
+      {marker && (
+        <Marker position={[marker.lat, marker.lng]}>
+          <Popup>
+            座標: {marker.lat}, {marker.lng}
+          </Popup>
+        </Marker>
+      )}
     </MapContainer>
   );
 }
